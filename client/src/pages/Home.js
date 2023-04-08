d05b6e58b438e99f5543bb783498a15fb4710743
shovan <karnashovan2@email.com>
2023-04-08 17:35:53 +0530

    Started The Front end Home Page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div className='homePageWrapper'>
+
+      <div className='formWrapper'>
+        <img src='/CodeSquadronLogo.png' alt='code-squadron-logo' />
+        <h4 className='mainLabel'>Paste Invitation Room Id:</h4>
+        <div className='inputGroup'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
